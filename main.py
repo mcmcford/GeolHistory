8cd51b4c0d7cf95f307bffd35967722a61bb4d3c
Morgan McFord (mcfordm) <mcmcford93@gmail.com>
2021-12-19 10:07:53 +0000

    starting conversion over to interactions opposed to discord py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import os
-import requests
+import sqlite3
 import datetime
 import traceback
-import configparser
-import sqlite3
 import numpy as np
-import xml.etree.ElementTree as ET
-import discord
-from discord.ext import commands
-from discord.ext.commands import Bot
-from discord import Member
-from discord.ext.commands import has_permissions, MissingPermissions
+import configparser
+import interactions
 
 config = configparser.ConfigParser()
 
@@ -28,8 +22,7 @@ config.read('config.ini')
 ##### Variables #####
 
 bot_token = config['DEFAULT']['bot_token']
-bots_prefix = "!"
-
+guilds = [713068366419722300,816000848445571073]
 #####################
 
 #check if database is made and load it
@@ -41,20 +34,16 @@ cursor.execute('CREATE TABLE IF NOT EXISTS rules(number TEXT primary key,  descr
 
 
 # setting bot prefix
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(bots_prefix))
-
-# remove default help command
-bot.remove_command('help')
+bot = interactions.Client(token=bot_token)
 
 @bot.event
 async def on_ready():
     """On ready event!"""
     print("Logged in as " + str(bot.user))
     print("User ID: " + str(bot.user.id))
-    await bot.change_presence(activity=discord.Game(name="!ghelp"))
     
 #ping commmand
-@bot.command(pass_context=True)
+@bot.command(name="Ping",description="This command will return the bots ping",scope=guilds)
 async def ping(ctx):
     latency = ctx.bot.latency
     latency = latency * 1000
@@ -416,7 +405,6 @@ async def addadmin(ctx):
             Send = await ctx.send("Please ensure you have formatted the command correctly\n`!addadmin @user` or `!addadmin 123456789012345678`")
     else:
         Send = await ctx.send("You don't have permission to use the command `addadmin`")
-
 
 #change the notification channel for the bot
 @bot.command()
@@ -795,153 +783,4 @@ async def config(ctx):
     else:
         Send = await ctx.send("You don't have permission to use the command `config`")
     
-#manually check for new article
-@bot.command()
-async def update(ctx):
-    
-    try:
-        #delete the senders message
-        await ctx.message.delete()
-    except Exception:
-        print("not deleting message due to it being in a DM")
-    
-    #get channel ID from db
-    cursor.execute("SELECT value_key FROM config WHERE value = ?",("notification_channel",))
-    channelID = cursor.fetchone()
-    channel = bot.get_channel(int(channelID[0])) # the message's channel
-    
-    #get admin IDs from db
-    cursor.execute("SELECT value_key FROM config WHERE value = ?",("admin_ids",))
-    admin_ids = cursor.fetchone()
-    admin_ids_array = str(admin_ids[0]).split(",")
-    
-    #get lead admins ID from db
-    cursor.execute("SELECT value_key FROM config WHERE value = ?",("lead_admin",))
-    leadAdmin = cursor.fetchone()
-    
-    #get owners ID from db
-    cursor.execute("SELECT value_key FROM config WHERE value = ?",("owner_id",))
-    ownerID = cursor.fetchone()
-    
-    allowed = False
-    owner = False
-    
-    for x in admin_ids_array:
-        if x == str((ctx.message.author).id):
-            allowed = True
-    
-    if str((ctx.message.author).id) == ownerID[0]:
-        allowed = True
-        owner = True
-    elif str((ctx.message.author).id) == leadAdmin[0]:
-        allowed = True
-    
-    if allowed == True:
-        
-        
-        # declare url
-        url = 'https://geolhistory.org/rss/articles.php'
-        
-        # http response
-        resp = requests.get(url)
-        
-        # save response
-        with open('feed.xml', 'wb') as f:
-            f.write(resp.content)
-        
-        # start parsing through the file by creating tree object
-        tree = ET.parse('feed.xml')
-        
-        # get root element
-        root = tree.getroot()
-        
-        # create empty list for news items
-        articles = []
-        
-        # go through each article in the XML file
-        for x in root[0].findall('item'):
-            title =x.find('title').text
-            link =x.find('link').text
-            URL = "\"" + str(link) + "\""
-            
-            desc =x.find('description').text
-            author =x.find('author').text
-            image = "https://service.lmwn.co.uk/brandkit/geolhistory/background-dinosaur-logo.png"
-            # once image is added to XML, it needs to be added to replace the above
-            
-            # checking if any part of the article is already in the database (bar the author as that will come up multiple times)
-            
-            is_title_already_in_db = False
-            is_link_already_in_db = False
-            is_desc_already_in_db = False
-            
-            
-            # checking title
-            cursor.execute("SELECT count(*) FROM articles WHERE title = ?",(title,))
-            find = cursor.fetchone()[0]
-            
-            if find>0:
-                is_title_already_in_db = True
-            
-
-            # checking link
-            cursor.execute("SELECT count(*) FROM articles WHERE link = ?",(URL,))
-            find = cursor.fetchone()[0]
-            
-            if find>0:
-                is_link_already_in_db = True
-            
-
-            # checking description
-            cursor.execute("SELECT count(*) FROM articles WHERE description = ?",(desc,))
-            find = cursor.fetchone()[0]
-            
-            if find>0:
-                is_desc_already_in_db = True
-            
-            # seeing how many values are already in the database and carrying out the appropriate actions
-            
-            if (is_title_already_in_db == False and is_link_already_in_db == False and is_desc_already_in_db == False):
-                #sending notification
-                
-                # modifying the XML description to remove copyright and URL at end
-                cut_description_array = desc.split("Read more at GeolHistory.org")
-                cut_description = cut_description_array[0]
-                
-                # removing email from author
-                cut_author_array = author.split("(")
-                cut_author = cut_author_array[1].rstrip((cut_author_array[1])[-1]) # remove the ending bracket from the sting
-                
-                
-                #building the embed
-                style = discord.Embed(name="Article Notification", title=title, description=cut_description, timestamp=datetime.datetime.utcnow(), color=0x28a4fd)
-                style.set_image(url=image)
-                style.add_field(name="Author", value=cut_author, inline=True)
-                style.add_field(name="To Read More", value="[Click Here](" + link + ")", inline=True)
-                style.set_footer(text = "Version 2.0.1")
-                
-                #get new article message from db
-                cursor.execute("SELECT value_key FROM config WHERE value = ?",("new_article_message",))
-                find = cursor.fetchone()
-                
-                
-                #sending the embed
-                Send = await channel.send(find[0], embed=style)
-                
-                #getting the message ID
-                message_id = Send.id
-                
-                
-                #insert into database
-                cursor.execute("INSERT INTO articles VALUES(?,?,?,?,?,?)",(message_id,title,URL,desc,author,"N/A"))
-                db.commit()
-            
-            elif (is_title_already_in_db == True and is_link_already_in_db == True and is_desc_already_in_db == False):
-                # update description
-                print("placeholder")
-    
-    else:
-        Send = await ctx.send("You don't have permission to use the command `update`")
-
-
-bot.run(bot_token) 
+bot.start()
